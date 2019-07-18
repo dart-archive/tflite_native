b3f3561b6647d93b070cb51ac49b858cc412ff1d
Ari Aye <ariaye@google.com>
2019-07-18 11:27:52 -0700

    Create initial working version

---
+++ lib/src/bindings/dlib.dart
@@ -0,0 +1,19 @@
+// Copyright (c) 2019, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:ffi';
+import 'dart:io';
+import 'package:path/path.dart' as p;
+
+String getPlatformLibraryName() {
+  if (Platform.isLinux) {
+    return 'libtensorflowlite_c-linux.so';
+  }
+
+  throw new Exception('Unsupported platform!');
+}
+
+/// TensorFlowLite C library.
+DynamicLibrary tflitelib = DynamicLibrary.open(
+    p.join(Directory.current.path, 'so', getPlatformLibraryName()));
