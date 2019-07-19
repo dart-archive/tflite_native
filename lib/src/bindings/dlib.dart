192ca9092634d3a85a632183cf80f2c6ca5b73ce
Ari Aye <ariaye@google.com>
2019-07-19 09:35:53 -0700

    Leverage Isolate.resolvePackageUri for finding .so file

--- lib/src/bindings/dlib.dart
+++ lib/src/bindings/dlib.dart
@@ -2,11 +2,13 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:cli' as cli;
 import 'dart:ffi';
 import 'dart:io';
+import 'dart:isolate' show Isolate;
 import 'package:path/path.dart' as path;
 
-String _getPlatformLibraryName() {
+String _getPlatformSpecificName() {
   if (Platform.isLinux) {
     return 'libtensorflowlite_c-linux.so';
   }
@@ -15,5 +17,10 @@ String _getPlatformLibraryName() {
 }
 
 /// TensorFlowLite C library.
-DynamicLibrary tflitelib = DynamicLibrary.open(
-    path.join(Directory.current.path, 'lib', _getPlatformLibraryName()));
+DynamicLibrary tflitelib = () {
+  final rootLibrary = 'package:tflite_native/tflite.dart';
+  final blobs = cli
+    .waitFor(Isolate.resolvePackageUri(Uri.parse('package:tflite_native/tflite.dart')))
+    .resolve('src/blobs/');
+  return DynamicLibrary.open(blobs.resolve(_getPlatformSpecificName()).toFilePath());
+}();
--- lib/src/ffi/helper.dart
+++ lib/src/ffi/helper.dart
@@ -3,6 +3,8 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'dart:ffi';
+import 'dart:isolate' show Isolate;
+import 'dart:cli' as cli;
 
 /// Forces casting a [Pointer] to a different type.
 ///
